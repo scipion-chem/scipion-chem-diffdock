fc1b1bd55fd6637463f3d3e6a6bae78dbde662af
Joaquin Algorta Bove <joaco.algorta@gmail.com>
2026-05-22 10:06:16 +0200

    fix issues

--- diffdock/__init__.py
+++ diffdock/__init__.py
@@ -99,7 +99,7 @@ class Plugin(pwchemPlugin):
 			f"sed -i '/inference_steps: 20/d' {yamlFile} && "
 			f"sed -i '/model_dir: \\.\\/workdir\\/v1.1\\/score_model/d' {yamlFile} && "
 			f"sed -i '/confidence_model_dir: \\.\\/workdir\\/v1.1\\/confidence_model/d' {yamlFile} && "
-			f"sed -i '/samples_per_complex: 5/d' {yamlFile}"
+			f"sed -i '/samples_per_complex: 10/d' {yamlFile}"
 		)
 		return installer.addCommand(cleanCmd, 'YAML_CLEANED')
 
